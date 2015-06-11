2de11077c065acb0a1715c51cabd551f834dbe8f
Eivin Landa <landa@drivdigital.no>
2015-06-12 00:04:04 +0200

    Added action buttons

--- scenes/game-battle.js
+++ scenes/game-battle.js
@@ -11,10 +11,15 @@
           html += '<div class=info--gold></div>';
         html += '</div>';
         html += '<div>battle-screen</div>';
-        html += '<div class=bar--life></div>';
-        html += '<div class=bar--mana></div>';
+        html += '<div class="bar-vertical bar--life"></div>';
+        html += '<div class="bar-vertical bar--mana"></div>';
         html += '<div class=bar--action-queue></div>';
-        html += '<div class=bar--actions></div>';
+        html += '<div class=bar--actions>';
+          html += '<div class="action action--heal">';
+          html += '<div class="action action--defend">';
+          html += '<div class="action action--attack">';
+          html += '<div class="action action--magic">';
+        html += '</div>';
         html += '<div class=squad-manager></div>';
         html += '<div class=squad-manager>squad-manager</div>';
         html += '<div class=flee-button></div>';
